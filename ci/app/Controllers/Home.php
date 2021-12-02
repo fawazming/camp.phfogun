5485ed8cd6087fdeb53a502dc13e859cd09e5ed2
fawazming <fawazming@gmail.com>
2021-12-02 09:23:46 +0100

    sns

---
+++ ci/app/Controllers/Home.php
@@ -0,0 +1,20 @@
+<?php namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	public function sms()
+	{
+		$incoming = $this->request->getPost();
+		$jsonObject = json_decode(implode('', $incoming));
+		// do_anything($jsonObject->subject, $jsonObject->message) // do_anything - function anything doing
+
+		var_dump($jsonObject->subject, $jsonObject->message);
+	}
+	//--------------------------------------------------------------------
+
+}
