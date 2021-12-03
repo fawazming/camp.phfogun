f7f337e6c0c697831326bb994a2dbd5436a94068
fawazming <fawazming@gmail.com>
2021-12-03 06:34:51 +0100

    form ui

--- ci/app/Controllers/Logic.php
+++ ci/app/Controllers/Logic.php
@@ -7,19 +7,12 @@ class Logic extends BaseController
 
 	public function index()
 	{
-		return view('welcome_message');
+		echo view('home');
 	}
 
 	public function sms()
 	{
 		$incoming = $this->request->getJSON();
-		// $jsonObject = json_decode(implode('', $incoming));
-		// do_anything($jsonObject->subject, $jsonObject->message) // do_anything - function anything doing
-
-		// var_dump($jsonObject->subject, $jsonObject->message);
-		// print_r($jsonObject->message);
-		// echo('done '.$incoming->subject.' ');
-		// print_r($incoming);
 		$data = [
 			'message'=> 'created'
 		];
