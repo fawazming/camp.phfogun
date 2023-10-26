f9d46bbc445cda7baa5bae4f522a14d608dc5819
fawazming <fawazming@gmail.com>
2023-10-26 10:47:00 -0600

    2023

---
+++ ci/app/Views/options.php
@@ -0,0 +1,17 @@
+        <h5>You are Welcome! PMC 2023 early bird ends <b>November 30</b><br>Early Bird 😊: ₦6,500 (Secondary School) || ₦7,500 (Post Sec. School & Undergraduate) || ₦9,500 (Others)  <br>
+        Standard 😟: ₦8,500 (Secondary School) || ₦10,000 (Post Sec. School & Undergraduate) || ₦12,000 (Others)  </h5>
+        <div class="row">
+            <a href="<?=base_url('buypin')?>" class="col border">
+                <h2>Buy Pin</h2>
+                <small>This is your first step</small>
+
+            </a>
+            <a href="<?=base_url('register')?>" class="col border">
+                <h2>Register</h2>
+                <small>I have pin, I want to register</small>
+            </a>
+            <a href="<?=base_url('pinstatus')?>" class="col border">
+                <h2>Status</h2>
+                <small>Is my pin valid?</small>
+            </a>
+        </div>
